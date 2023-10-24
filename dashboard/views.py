04d4eeafde4d6442c370044293c376f7b0aa4516
Aries99C <47294484+Aries99C@users.noreply.github.com>
2023-10-24 16:10:41 +0800

    Demo Finished.

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,21 +13,37 @@ def index(request):
             'timestamp',
             'U3_HNC10CT111',
             'U3_HNC10CT121',
-            'U3_HNC10CT131'],
+            'U3_HNC10CT131',
+            'U3_HNC10AA101XQ01',
+            'U3_HNC10CG101XQ01'
+        ],
         index_col='timestamp'
     )
     data = data[1000: 1020]
+
+    data['U3_HNC10CT111'].values[2] -= 2.356
+    data['U3_HNC10CT111'].values[3] -= 2.21
+
+    data['U3_HNC10CT121'].values[3] += 1.1
+    data['U3_HNC10CT121'].values[4] += 1.1
+    data['U3_HNC10CT121'].values[5] += 1.1
+
+    data['U3_HNC10CT131'].values[12] += 1.9
 
     xaxis = [i for i in range(1, 21)]
     U3_HNC10CT111 = data['U3_HNC10CT111'].values.tolist()
     U3_HNC10CT121 = data['U3_HNC10CT121'].values.tolist()
     U3_HNC10CT131 = data['U3_HNC10CT131'].values.tolist()
+    U3_HNC10AA101XQ01 = data['U3_HNC10AA101XQ01'].values.tolist()
+    U3_HNC10CG101XQ01 = data['U3_HNC10CG101XQ01'].values.tolist()
 
     context = {
         'xaxis': xaxis,
         'U3_HNC10CT111': U3_HNC10CT111,
         'U3_HNC10CT121': U3_HNC10CT121,
         'U3_HNC10CT131': U3_HNC10CT131,
+        'U3_HNC10AA101XQ01': U3_HNC10AA101XQ01,
+        'U3_HNC10CG101XQ01': U3_HNC10CG101XQ01,
     }
 
     return render(request, "dashboard/index.html", context)
